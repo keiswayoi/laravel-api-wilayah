fbc8949b4167751d88fac9779f468b57a0fb4bb6
vozaldi <vozaldi@gmail.com>
2024-03-12 11:13:07 +0700

    Fix sql migrate

---
+++ database/migrations/2024_03_12_034544_create_subdistricts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subdistricts', function (Blueprint $table) {
+            $table->id();
+            $table->string('provinsi');
+            $table->string('kota_kabupaten');
+            $table->string('kecamatan');
+            $table->string('kelurahan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subdistricts');
+    }
+};
